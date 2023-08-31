0626ce863d1d9d9cef90dce48a60048638585b2f
Cade Brown <brown.cade@gmail.com>
2023-08-30 22:09:43 -0400

    title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Alkemical Development';
+export const SITE_DESCRIPTION = 'Open Research Lab for Artificial Intelligence';
